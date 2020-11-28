eb0f1c7d240090e662ab9918c448b78044639043
Nirbhay Sibal <nirbhay.sibal@match.com>
2020-11-27 23:50:41 -0600

    Create split-line.js

---
+++ src/split-line.js
@@ -0,0 +1,25 @@
+/*
+input:
+line: [String]
+width: Number
+
+Output:
+[[String], [String]]
+*/
+
+module.exports = function splitLine(line, width){
+  if (line.length === 0) return [[], []];
+
+  let [head, ...rest] = line;
+
+  if (width === 0 || head.length > width) return [[], line];
+
+  let firstHalf = [head];
+  let secondHalf = [];
+
+  let [first, second] = splitLine(rest, width - (head.length + 1));
+  first.forEach(word => firstHalf.push(word));
+  second.forEach(word => secondHalf.push(word));
+
+  return [firstHalf, secondHalf];
+}
